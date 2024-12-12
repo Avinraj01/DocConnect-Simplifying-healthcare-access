d116896a53021c13daf3503738388d8b0067776f
AVIN RAJ <avinraj.jamia@gmail.com>
2024-12-12 05:36:08 +0530

    Create source code- schedule.cpp

---
+++ source code- schedule.cpp
@@ -0,0 +1,13 @@
+//DocConnect
+#include "Schedule.h"
+
+Schedule::Schedule(const std::string& date, const std::string& time)
+    : date(date), time(time) {}
+
+const std::string& Schedule::getDate() const {
+    return date;
+}
+
+const std::string& Schedule::getTime() const {
+    return time;
+}
